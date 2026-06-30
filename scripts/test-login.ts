509d1b149f05f380aee43e1cdcd23ddea1467393
Md Tanvir Ahamed Shanto <mdtanvirahamdsanto987@gmail.com>
2026-07-01 00:58:56 +0600

    chore: remove obsolete utility scripts for database testing and superadmin management

--- scripts/test-login.ts
+++
@@ -1,9 +0,0 @@
-import 'dotenv/config';
-import { connectMongo, UserModel } from '@devlock/database';
-async function test() {
-  await connectMongo(process.env.MONGODB_URI?.replace('?replicaSet=rs0', ''));
-  const user = await UserModel.findOne({ email: 'admin@gmail.com' }).lean();
-  console.log('User from DB:', user);
-  process.exit(0);
-}
-test();
